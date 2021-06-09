e8b0061bf39981c179025815d3da6e30755e3eab
saskasaska <oldstep13@gmail.com>
2021-06-09 17:44:13 +0300

    add some assets

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,26 +1,42 @@
-import adminIcon from '../assets/administration.svg';
-import logoIcon from '../assets/logo.png';
-import homeIcon from '../assets/house.svg';
-import blackHouseIcon from '../assets/house2.svg';
-import arrowDownIcon from '../assets/arrow-down.svg';
-import searchIcon from '../assets/search.svg';
-import commentsIcon from '../assets/comments.svg';
-import bellIcon from '../assets/bell.svg';
-import networkIcon from '../assets/network.svg';
-import yourPublicationsIcon from '../assets/publications.svg';
-import ecosystemIcon from '../assets/ecosystem.svg';
-import entitiesIcon from '../assets/entities.svg';
-import userPlusIcon from '../assets/user-plus.svg';
-import plusIcon from '../assets/plus.svg';
-import peopleIcon from '../assets/people.svg';
-import logoutIcon from '../assets/logout.svg';
-import bookIcon from '../assets/book-solid.svg';
-import entities2Icon from '../assets/entities2.svg';
-import contractIcon from '../assets/edit-regular.svg';
-import privacyIcon from '../assets/privacy.svg';
-import settingsIcon from '../assets/settings.svg';
-import userIcon from '../assets/superhero.png';
-import background from '../assets/background.svg';
+import adminIcon from './icons/administration.svg';
+import logoIcon from './icons/logo.png';
+import homeIcon from './icons/house.svg';
+import blackHouseIcon from './icons/house2.svg';
+import arrowDownIcon from './icons/arrow-down.svg';
+import searchIcon from './icons/search.svg';
+import commentsIcon from './icons/comments.svg';
+import bellIcon from './icons/bell.svg';
+import networkIcon from './icons/network.svg';
+import yourPublicationsIcon from './icons/publications.svg';
+import ecosystemIcon from './icons/ecosystem.svg';
+import entitiesIcon from './icons/entities.svg';
+import userPlusIcon from './icons/user-plus.svg';
+import plusIcon from './icons/plus.svg';
+import peopleIcon from './icons/people.svg';
+import logoutIcon from './icons/logout.svg';
+import bookIcon from './icons/book-solid.svg';
+import entities2Icon from './icons/entities2.svg';
+import contractIcon from './icons/edit-regular.svg';
+import privacyIcon from './icons/privacy.svg';
+import settingsIcon from './icons/settings.svg';
+import userIcon from './icons/man.svg';
+import messageIcon from './icons/message.svg';
+import requestIcon from './icons/request.svg';
+import suitcaseIcon from './icons/suitcase.svg';
+import closeIcon from './icons/close.svg';
+import editIcon from './icons/edit.svg';
+import mosaicIcon from './icons/mosaic.svg';
+import listIcon from './icons/list.svg';
+import allIcon from './icons/all.svg';
+import sortIcon from './icons/sort.svg';
+import shareIcon from './icons/share.svg';
+import followedIcon from './icons/followed.svg';
+import fullIcon from './icons/full.svg';
+import filterIcon from './icons/filter.svg';
+import dotsIcon from './icons/dots.svg';
+import calendarIcon from './icons/calendar.svg';
+import structureIcon from './icons/structure.svg';
+
 
 export const Icons ={
  adminIcon,
@@ -45,7 +61,22 @@ export const Icons ={
  privacyIcon,
  settingsIcon,
  userIcon,
- background,
+ messageIcon,
+ requestIcon,
+ suitcaseIcon,
+ editIcon,
+ closeIcon,
+ mosaicIcon,
+ listIcon,
+ allIcon,
+ followedIcon,
+ sortIcon,
+ shareIcon,
+ fullIcon,
+ filterIcon,
+ dotsIcon,
+ calendarIcon,
+ structureIcon,
 };
 
 export default Icons;
